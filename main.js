8a05fac49cf66233205dfb8cc95019edd40d075b
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-01-17 11:31:30 -0500

    Electron y electron-builder son configurados

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+
+let appWin;
+
+createWindow = () => {
+    appWin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "Pruebas de Electron",
+        resizable: false,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        }
+    });
+
+    appWin.loadURL(url.format({
+        pathname: path.join(__dirname, "dist/browser/index.html"),
+        protocol: "file",
+        slashes: true
+    }));
+
+    appWin.setMenu(null);
+
+    appWin.on("closed", () => {
+        appWin = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+});
