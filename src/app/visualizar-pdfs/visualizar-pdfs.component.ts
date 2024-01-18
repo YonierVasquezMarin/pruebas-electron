780a93ae3c68095e8b7ed476d44633038651b3b5
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-01-17 21:50:07 -0500

    componente visualizar pdfs es agregado

---
+++ src/app/visualizar-pdfs/visualizar-pdfs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-pdfs',
+  standalone: true,
+  imports: [],
+  templateUrl: './visualizar-pdfs.component.html',
+  styleUrl: './visualizar-pdfs.component.scss'
+})
+export class VisualizarPdfsComponent {
+
+}
