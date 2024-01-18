2115603b03dd51eb422919cc86166e6d1f3f5a7e
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-01-17 22:22:15 -0500

    funcion iniciada para ver pdfs

--- src/app/visualizar-pdfs/visualizar-pdfs.component.ts
+++ src/app/visualizar-pdfs/visualizar-pdfs.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { dialog } from 'electron';
 
 @Component({
-  selector: 'app-visualizar-pdfs',
-  standalone: true,
-  imports: [],
-  templateUrl: './visualizar-pdfs.component.html',
-  styleUrl: './visualizar-pdfs.component.scss'
+    selector: 'app-visualizar-pdfs',
+    standalone: true,
+    imports: [],
+    templateUrl: './visualizar-pdfs.component.html',
+    styleUrl: './visualizar-pdfs.component.scss',
 })
 export class VisualizarPdfsComponent {
-
+    abrirPdfs() {
+        dialog.showOpenDialog({
+            properties: ['openFile', 'multiSelections'],
+            filters: [{ name: 'PDFs', extensions: ['pdf'] }],
+        });
+    }
 }
