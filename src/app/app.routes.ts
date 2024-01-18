c3a6888f1ab3cdd474369a710020fa05438c2903
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-01-17 21:35:15 -0500

    Componente pagina principal es creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: PaginaPrincipalComponent,
+    },
+    {
+        path: 'pagina-principal',
+        component: PaginaPrincipalComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'pagina-principal',
+    },
+];
