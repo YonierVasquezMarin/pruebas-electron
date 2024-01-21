6e99544c51d287c15be170fc4abe9286feed35d5
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-01-20 21:18:17 -0500

    Se muestra el pdf seleccionado

--- src/app/components/visualizar-pdfs/visualizar-pdfs.component.ts
+++ src/app/components/visualizar-pdfs/visualizar-pdfs.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { LocalFileUrlSafePipe } from './../../pipes/local-file-url-safe.pipe'
+import { NgIf } from '@angular/common';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 @Component({
     selector: 'app-visualizar-pdfs',
     standalone: true,
-    imports: [],
+    imports: [NgIf, LocalFileUrlSafePipe],
     templateUrl: './visualizar-pdfs.component.html',
     styleUrl: './visualizar-pdfs.component.scss',
 })
 export class VisualizarPdfsComponent {
-    constructor() {}
+    constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
-    async abrirPdf() {
+    rutaPdf!: string;
+
+    abrirPdf() {
         window.ipcRenderer.send('abrir-pdf');
-        window.ipcMain.on('abrir-pdf', (event: any, arg: any) => {
-            console.log(arg);
+        window.ipcMain.on('abrir-pdf', (event: any, arg: string) => {
+            this.rutaPdf = arg;
+            this.changeDetectorRef.detectChanges();
         });
     }
 }
