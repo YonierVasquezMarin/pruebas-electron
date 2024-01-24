a5d6210b8df5eaa01aca6ec9d3ecc87401a4bba6
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-01-24 15:13:05 -0500

    servicio implementado para usar el sistema de electron

--- src/app/components/visualizar-pdfs/visualizar-pdfs.component.ts
+++ src/app/components/visualizar-pdfs/visualizar-pdfs.component.ts
@@ -1,6 +1,7 @@
 import { LocalFileUrlSafePipe } from './../../pipes/local-file-url-safe.pipe';
 import { NgIf } from '@angular/common';
 import { Component, ChangeDetectorRef } from '@angular/core';
+import { OpenFileDialogService } from '../../shared/services/open-file-dialog.service';
 
 @Component({
     selector: 'app-visualizar-pdfs',
@@ -10,19 +11,23 @@ import { Component, ChangeDetectorRef } from '@angular/core';
     styleUrl: './visualizar-pdfs.component.scss',
 })
 export class VisualizarPdfsComponent {
-    constructor(private changeDetectorRef: ChangeDetectorRef) {}
+    constructor(
+        private changeDetectorRef: ChangeDetectorRef,
+        private openFileDialogService: OpenFileDialogService
+    ) {}
 
     rutaPdf!: string;
 
-    abrirPdf() {
-        window.ipcRenderer.send('open-file-dialog', {
-            properties: ['openFile'],
-            filters: [{ name: 'PDF', extensions: ['pdf'] }],
-        });
-
-        window.ipcRenderer.on('file-dialog-opened', (event: any, arg: string) => {
-            this.rutaPdf = arg[0];
-            this.changeDetectorRef.detectChanges();
-        });
+    async abrirPdf() {
+        try {
+            const filesSelected =
+                await this.openFileDialogService.openFileDialog({
+                    properties: ['openFile'],
+                    filters: [{ name: 'PDF', extensions: ['pdf'] }],
+                });
+            this.rutaPdf = filesSelected.result[0];
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
